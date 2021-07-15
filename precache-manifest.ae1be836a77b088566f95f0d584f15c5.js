77b2eb05afbcfcf839dce2cfd7d0fa9e2489dfb8
Kulyk Oleksandr <70694467+akylik@users.noreply.github.com>
2021-07-15 11:16:58 +0300

    Updates

---
+++ precache-manifest.ae1be836a77b088566f95f0d584f15c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660c4e6d21dde84335081717da1660b5",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/index.html"
+  },
+  {
+    "revision": "80f01654cf0fa9203d61",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/css/2.1a215020.chunk.css"
+  },
+  {
+    "revision": "290c4cdfeaeb8e69ae22",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/css/main.e14d8429.chunk.css"
+  },
+  {
+    "revision": "80f01654cf0fa9203d61",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/js/2.821a421d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/js/2.821a421d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290c4cdfeaeb8e69ae22",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/js/main.46153ea7.chunk.js"
+  },
+  {
+    "revision": "4c2371be182cde6f93db",
+    "url": "/phonebooke_react_redux_redux-thunk_reselect/static/js/runtime-main.f856693f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/phonebooke_react_redux_redux-thunk_reselect/precache-manifest.ae1be836a77b088566f95f0d584f15c5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phonebooke_react_redux_redux-thunk_reselect/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
